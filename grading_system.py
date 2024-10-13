5587adb48abe47bea0d4a747b2d0dde6c3f21def
ajmainwasif01 <nipunazmun@gmail.com>
2024-10-13 16:20:22 +0600

    Grading System

---
+++ grading_system.py
@@ -0,0 +1,30 @@
+a = int(input("Enter the mark of first subject :"))
+b = int(input("Enter the mark of second subject :"))
+c = int(input("Enter the mark of third subject :"))
+d = int(input("Enter the mark of fourth subject :"))
+e = int(input("Enter the mark of fifth subject :"))
+grade = (a + b + c + d + e) / 5
+
+if  80 <= grade <= 100:
+ print("Your grade is A+")
+
+elif  70 <= grade <=79:
+ print("Your grade is A")
+
+elif  60 <= grade <=69:
+ print("Your grade is Agrade <=")
+
+elif  50 <= grade <=59:
+ print("Your grade is B")
+
+elif  40 <= grade <=49:
+ print("Your grade is c")
+ 
+elif  33 <= grade <=39:
+ print("Your grade is D")
+
+else :
+ print("Your grade is F")
+
+ 
+
